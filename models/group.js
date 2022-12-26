0c95b616aeab22b4d3b97c4d75d7ff8a79ad3873
divyanshigkmit <divyanshi@gkmit.co>
2022-12-26 23:26:47 +0530

    Create model of group table

---
+++ models/group.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Group extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Group.init({
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    category: {
+      type: Sequelize.ENUM,
+      values: ['trip', 'home', 'couple', 'other']
+    }
+  }, {
+    sequelize,
+    paranoid: true,
+    tableName: 'group',
+    modelName: 'Group',
+  });
+  return Group;
+};
