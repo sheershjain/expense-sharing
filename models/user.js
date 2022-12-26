2b7a3b606fe6de24d94864a971fdc67c2f48f83e
divyanshigkmit <divyanshi@gkmit.co>
2022-12-26 22:43:43 +0530

    Create database connectivity

---
+++ models/user.js
@@ -0,0 +1,46 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    first_name: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    last_name: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    google_id: {
+      type: Sequelize.STRING,
+      allowNull: true,
+      unique: true
+    },
+  }, {
+    sequelize,
+    paranoid: true,
+    tableName: 'user',
+    modelName: 'User',
+  });
+  return User;
+};
