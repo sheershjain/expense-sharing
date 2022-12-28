3c84be18e509c2d0e146e35ed16582de0e6f878f
divyanshigkmit <divyanshi@gkmit.co>
2022-12-28 13:11:30 +0530

    Create group user mapping model

---
+++ models/group-user-mapping.js
@@ -0,0 +1,45 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class GroupUserMapping extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.belongsTo(models.User, {
+        foreignKey: 'user_id'
+      });
+      this.belongsTo(models.Group, {
+        foreignKey: 'group_id'
+      });
+    }
+  }
+  GroupUserMapping.init({
+    groupId: {
+      allowNull: true,
+      type: Sequelize.UUID,
+      references: {
+        model: 'group',
+        key: 'id',
+      }
+    },
+    userId: {
+      allowNull: false,
+      type: Sequelize.UUID,
+      references: {
+        model: 'user',
+        key: 'id',
+      }
+    }
+  }, {
+    sequelize,
+    paranoid: true,
+    tableName: 'group_user_mapping',
+    modelName: 'GroupUserMapping',
+  });
+  return GroupUserMapping;
+};
