9ab4fb0712a38b2b9066a4b6cdcc194fd26df13f
divyanshigkmit <divyanshi@gkmit.co>
2022-12-28 15:12:46 +0530

    Update group id in transaction

--- migrations/20221227024156-create-transaction.js
+++ migrations/20221227024156-create-transaction.js
@@ -1,69 +1,72 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('transaction', {
+    await queryInterface.createTable("transaction", {
       id: {
         allowNull: false,
         primaryKey: true,
         type: Sequelize.UUID,
-        defaultValue: Sequelize.literal('uuid_generate_v4()')
+        defaultValue: Sequelize.literal("uuid_generate_v4()"),
       },
       name: {
         type: Sequelize.STRING,
-        allowNull: false
+        allowNull: false,
       },
       payee_id: {
         allowNull: false,
         type: Sequelize.UUID,
         references: {
-          model: 'user',
-          key: 'id',
-        }
+          model: "user",
+          key: "id",
+        },
       },
       payer_id: {
         allowNull: false,
         type: Sequelize.UUID,
         references: {
-          model: 'user',
-          key: 'id',
-        }
+          model: "user",
+          key: "id",
+        },
       },
       amount: {
         type: Sequelize.FLOAT,
-        allowNull: true
+        allowNull: true,
       },
       split_type: {
         type: Sequelize.ENUM,
-        values: ['equally', 'unequally', 'exactly']
+        values: ["equally", "unequally", "exactly"],
       },
       group_id: {
         allowNull: true,
         type: Sequelize.UUID,
-        defaultValue: Sequelize.literal('uuid_generate_v4()')
+        references: {
+          model: "group",
+          key: "id",
+        },
       },
       is_settle: {
         type: Sequelize.BOOLEAN,
-        defaultValue: false
+        defaultValue: false,
       },
       created_at: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.NOW
+        defaultValue: Sequelize.NOW,
       },
       updated_at: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.NOW
+        defaultValue: Sequelize.NOW,
       },
       deleted_at: {
         allowNull: true,
         type: Sequelize.DATE,
-        defaultValue: null
-      }
+        defaultValue: null,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('transaction');
-  }
+    await queryInterface.dropTable("transaction");
+  },
 };
