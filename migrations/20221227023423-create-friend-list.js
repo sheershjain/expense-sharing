5986f68c735fa9eddfc67cbc89abbb019c0e625a
divyanshigkmit <divyanshi@gkmit.co>
2022-12-29 08:19:13 +0530

    Update
    Remove status column from friend list model

--- migrations/20221227023423-create-friend-list.js
+++ migrations/20221227023423-create-friend-list.js
@@ -1,13 +1,13 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('friend_list', {
+    await queryInterface.createTable("friend_list", {
       id: {
         allowNull: false,
         primaryKey: true,
         type: Sequelize.UUID,
-        defaultValue: Sequelize.literal('uuid_generate_v4()')
+        defaultValue: Sequelize.literal("uuid_generate_v4()"),
       },
       friend_one: {
         allowNull: false,
@@ -32,21 +32,21 @@ module.exports = {
       created_at: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.NOW
+        defaultValue: Sequelize.NOW,
       },
       updated_at: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.NOW
+        defaultValue: Sequelize.NOW,
       },
       deleted_at: {
         allowNull: true,
         type: Sequelize.DATE,
-        defaultValue: null
-      }
+        defaultValue: null,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('friend_list');
-  }
+    await queryInterface.dropTable("friend_list");
+  },
 };
