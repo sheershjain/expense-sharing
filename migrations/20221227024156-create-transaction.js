c2f39c859d20f14c899432184388067197559d04
divyanshigkmit <divyanshi@gkmit.co>
2022-12-29 08:38:20 +0530

    Update
    Rename amount->baseAmount and add amountToPay in transaction model

--- migrations/20221227024156-create-transaction.js
+++ migrations/20221227024156-create-transaction.js
@@ -2,65 +2,69 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('transaction', {
+    await queryInterface.createTable("transaction", {
       id: {
         allowNull: false,
         primaryKey: true,
         type: Sequelize.UUID,
-        defaultValue: Sequelize.literal('uuid_generate_v4()')
+        defaultValue: Sequelize.literal("uuid_generate_v4()"),
       },
       name: {
         type: Sequelize.STRING,
-        allowNull: false
+        allowNull: false,
+      },
+      base_amount: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
       },
       payee_id: {
         allowNull: false,
         type: Sequelize.UUID,
         references: {
-          model: 'user',
-          key: 'id',
-        }
+          model: "user",
+          key: "id",
+        },
       },
       payer_id: {
         allowNull: false,
         type: Sequelize.UUID,
         references: {
-          model: 'user',
-          key: 'id',
-        }
+          model: "user",
+          key: "id",
+        },
       },
-      amount: {
-        type: Sequelize.FLOAT,
-        allowNull: true
+      amount_to_pay: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
       },
       split_type: {
         type: Sequelize.ENUM,
-        values: ['equally', 'unequally', 'exactly']
+        values: ["equally", "unequally", "exactly"],
       },
       group_id: {
         allowNull: true,
         type: Sequelize.UUID,
-        defaultValue: Sequelize.literal('uuid_generate_v4()')
+        defaultValue: Sequelize.literal("uuid_generate_v4()"),
       },
       is_settle: {
         type: Sequelize.BOOLEAN,
-        defaultValue: false
+        defaultValue: false,
       },
       created_at: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.NOW
+        defaultValue: Sequelize.NOW,
       },
       updated_at: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.NOW
+        defaultValue: Sequelize.NOW,
       },
       deleted_at: {
         allowNull: true,
         type: Sequelize.DATE,
-        defaultValue: null
-      }
+        defaultValue: null,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
--- models/transaction.js
+++ models/transaction.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model, Sequelize
-} = require('sequelize');
+"use strict";
+const { Model, Sequelize } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Transaction extends Model {
     /**
@@ -11,58 +9,65 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       this.belongsTo(models.User, {
-        foreignKey: 'payee_id',
-        targetKey: 'id'
+        foreignKey: "payee_id",
+        targetKey: "id",
       });
       this.belongsTo(models.User, {
-        foreignKey: 'payer_id',
-        targetKey: 'id'
+        foreignKey: "payer_id",
+        targetKey: "id",
       });
     }
   }
-  Transaction.init({
-    name: {
-      type: Sequelize.STRING,
-      allowNull: false
+  Transaction.init(
+    {
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      baseAmount: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      payeeId: {
+        allowNull: false,
+        type: Sequelize.UUID,
+        references: {
+          model: "user",
+          key: "id",
+        },
+      },
+      payerId: {
+        allowNull: false,
+        type: Sequelize.UUID,
+        references: {
+          model: "user",
+          key: "id",
+        },
+      },
+      amountToPay: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      splitType: {
+        type: Sequelize.ENUM,
+        values: ["equally", "unequally", "exactly"],
+      },
+      groupId: {
+        allowNull: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.literal("uuid_generate_v4()"),
+      },
+      isSettle: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
     },
-    payeeId: {
-      allowNull: false,
-      type: Sequelize.UUID,
-      references: {
-        model: 'user',
-        key: 'id',
-      }
-    },
-    payerId: {
-      allowNull: false,
-      type: Sequelize.UUID,
-      references: {
-        model: 'user',
-        key: 'id',
-      }
-    },
-    amount: {
-      type: Sequelize.FLOAT,
-      allowNull: true
-    },
-    splitType: {
-      type: Sequelize.ENUM,
-      values: ['equally', 'unequally', 'exactly']
-    },
-    groupId: {
-      allowNull: true,
-      type: Sequelize.UUID,
-      defaultValue: Sequelize.literal('uuid_generate_v4()')
-    },
-    isSettle: {
-      type: Sequelize.BOOLEAN,
-      defaultValue: false
+    {
+      sequelize,
+      paranoid: true,
+      tableName: "transaction",
+      modelName: "Transaction",
     }
-  }, {
-    sequelize,
-    paranoid: true,
-    tableName: 'transaction',
-    modelName: 'Transaction',
-  });
+  );
   return Transaction;
 };
